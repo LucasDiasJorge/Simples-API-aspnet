1f97eb7bd11fecd62318c358d29adae74307b8c5
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2025-06-14 22:17:19 -0300

    Creating Unit Tests

---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using API.Models;
+using Xunit;
+
+// https://learn.microsoft.com/en-us/visualstudio/test/walkthrough-creating-and-running-unit-tests-for-managed-code?view=vs-2022#create-a-project-to-test
+// https://learn.microsoft.com/en-us/dotnet/core/testing/unit-testing-best-practices
+public class UserTests
+{
+    [Fact]
+    public void CreateUser_ShouldInitializeCorrectly()
+    {
+        // Arrange
+        Company company = new Company { Name = "CompanyTest" };
+
+        // Act
+        User user = new User
+        {
+            Name = "UserNameTest",
+            Email = "useremail@test.com",
+            Password = "nonHashedPass",
+            Company = company
+        };
+
+        // Assert
+        Assert.NotNull(user);
+        Assert.Equal("UserNameTest", user.Name);
+        Assert.Equal("useremail@test.com", user.Email);
+        Assert.Equal("nonHashedPass", user.Password);
+        Assert.NotNull(user.Company);
+        Assert.Equal("CompanyTest", user.Company.Name);
+    }
+}
